e7e3f7dd09b54dfb8a04b81f21d88e04188d5359
TNThacker2015 <tnthacker2015@gmail.com>
2020-02-10 22:03:01 -0800

    e

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,163 @@
-(async () => {
-  const addElectric = (i: number) =>
-    (localStorage.electric = +localStorage.electric + i);
-  setInterval(() => {
-    if (!localStorage.electric) localStorage.electric = 0;
-    document.getElementById("bois")!.innerText = `Bois: ${
-      localStorage.electric
-    }`;
-  });
-  const electricboi = document.getElementById("eleboi");
-  if (!electricboi) return;
-  electricboi.addEventListener("click", () => {
-    addElectric(1);
-  });
-})();
+import { toWords } from "./words";
+import Swal from "sweetalert2";
+const entries = Object.entries as <T>(
+	o: T
+) => [Extract<keyof T, string>, T[keyof T]][];
+window.onload = async () => {
+	
+const defaults: Partial<Store> = {
+	electric: BigInt(0)
+};
+const store = new Proxy(Object.create(null) as Store, {
+	get(t, k) {
+		const val = localStorage[k as any];
+		if (!val) return;
+		if (typeof defaults[k as keyof Store] === "bigint")
+			return BigInt(val);
+		try {
+			return JSON.parse(val);
+		} catch {
+			return val;
+		}
+	},
+	set(t, k, v) {
+		try {
+			localStorage[k as any] = JSON.stringify(v);
+		} catch {
+			localStorage[k as any] = v;
+		}
+		return true;
+	}
+});
+const appliances = new Proxy(Object.create(null) as Appliances, {
+	get(t, k) {
+		return store.appliances[k as keyof Appliances] ?? 0;
+	},
+	set(t, k, v) {
+		const app = store.appliances;
+		app[k as keyof Appliances] = v;
+		store.appliances = app;
+		return true;
+	}
+});
+console.log(appliances)
+	const electricboi = document.getElementById("eleboi");
+	const bois = document.getElementById("bois");
+	const smallBois = document.getElementById("smallbois");
+	const upgrades = document.getElementById("upgrades");
+	if (!(electricboi && bois && smallBois && upgrades))
+		return (document.body.innerHTML = "err");
+	type ApplianceCosts = {
+		[e in keyof Partial<Appliances>]: bigint;
+	};
+	interface Store {
+		electric: bigint;
+		appliances: Partial<Appliances>;
+		applianceCosts: ApplianceCosts;
+	}
+	
+	interface Appliances {
+		computer: number;
+	}
+	if (!store.appliances) store.appliances = {};
+	if (!store.applianceCosts) store.applianceCosts = {};
+	if (!store.electric) store.electric = BigInt(0);
+	const applianceCosts = new Proxy(Object.create(null) as ApplianceCosts, {
+		get(t, k) {
+			const cost = store.applianceCosts[k as keyof ApplianceCosts];
+			return (cost) && BigInt(cost);
+		},
+		set(t, k, v) {
+			const app = store.applianceCosts;
+			(app[k as keyof ApplianceCosts] as any) = Number(v);
+			store.applianceCosts = app;
+			return true;
+		}
+	});
+	for (const [k, v] of entries(defaults))
+		v &&
+			store[k as keyof Store] === undefined &&
+			(store[k as keyof Store] = v as any);
+	setInterval(() => {
+		bois.innerText = `Electric Bois: ${store.electric}`;
+		smallBois.innerText = toWords(store.electric);
+	});
+	electricboi.addEventListener("click", () => {
+		store.electric += BigInt(1);
+	});
+	const apps: {
+		[e in keyof Partial<Appliances>]: {
+			cost: bigint;
+			elem: HTMLParagraphElement;
+			countElem: HTMLParagraphElement;
+		};
+	} = {};
+	const addAppliance = (
+		name: keyof Appliances,
+		display: string,
+		desc: string,
+		baseCost = BigInt(10),
+		costMult: number | bigint | ((arg: bigint) => number | bigint)
+	) => {
+		const app = document.createElement("div");
+		app.classList.add("appliance");
+		const r = document.createElement("p");
+		r.classList.add("count");
+		r.innerText = String(store.appliances[name]);
+		app.append(r);
+		const n = document.createElement("p");
+		const f = document.createElement("div");
+		f.classList.add("labels");
+		app.append(f);
+		n.classList.add("label");
+		n.innerText = display;
+		f.append(n);
+		const e = document.createElement("p");
+		e.classList.add("desc");
+		e.innerText = desc;
+		f.append(e);
+		const c = document.createElement("p");
+		c.classList.add("desc");
+		c.classList.add("cost");
+		c.innerText = String(baseCost);
+		f.append(c);
+		apps[name] = {
+			get cost() {
+				return (
+					applianceCosts[name] ?? (applianceCosts[name] = baseCost)
+				);
+			},
+			set cost(cost) {
+				applianceCosts[name] = cost;
+			},
+			elem: c,
+			countElem: r
+		};
+		const obj = apps[name]!;
+		const buy = document.createElement("button");
+		buy.classList.add("buy");
+		buy.innerText = "BUY";
+		buy.onclick = () => {
+			if (store.electric < obj.cost) return Swal.fire("Not Enough Electric Bois", "You do not have enough electric bois to buy this.", "error");
+			else {
+				appliances[name] += 1;
+				store.electric -= obj.cost;
+				obj.cost =
+					typeof costMult === "bigint"
+						? obj.cost * costMult
+						: typeof costMult === "number" ? BigInt(Math.ceil(Number(obj.cost) * costMult)): BigInt(costMult(obj.cost));
+			}
+		};
+		app.append(buy);
+		upgrades.append(app);
+	};
+	addAppliance("computer", "Computer", "+0.5 CPS", BigInt(100), 1.55);
+	setInterval(() => {
+		for (const r of Object.values(apps))
+			r!.elem.innerText = String(r!.cost);
+		for (const [f, r] of Object.entries(apps))
+			r!.countElem.innerText = String(appliances[f as keyof Appliances] ?? 0);
+	});
+	setTimeout(() => store.electric || Swal.fire("Click!", "Click the electric boi gif to gain electric bois!", "info"), 8000)
+	setInterval(() => (store.electric += BigInt(appliances.computer)), 2000);
+};
