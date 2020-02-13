c33a0a6118a937f5c19e9a28f74125e4ed0883a7
TNThacker2015 <37024464+TNThacker2015@users.noreply.github.com>
2020-02-13 11:58:36 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ window.onload = async () => {
 		setTimeout(async()=>{
 			const h = (new Date()).getHours();
 			if (h >= 9 && h <=15) setInterval(async()=>{await Swal.fire({title:"Do your work!", text:"Stop playing games", icon:"error",allowOutsideClick: false})})
-		},10000)
+		},2000)
 
 		
 		//// const enabled = JSON.parse(await (await fetch("/enabled")).text());
